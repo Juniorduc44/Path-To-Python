d32d944b4cb5af9e6376c3dad032e280fc506849
Juniorduc44 <86094465+Juniorduc44@users.noreply.github.com>
2022-11-15 13:01:17 -0800

    Update Name_Item_count_tracker.py

--- Name_Item_count_tracker.py
+++ Name_Item_count_tracker.py
@@ -1,4 +1,7 @@
+from time import sleep
 
+print("When you are done listing names or items, dont forget to type 'done' in order to move forward with the app.\n")
+sleep(2)
 t = False #placed this here to allow initiation of while loop
 counts = dict()
 names = []
@@ -9,10 +12,15 @@ while t == False :
     if Counts == "done": break #this breaks us out of while loop from input queries
 
 #This section will do the counting of our names or items that show up in our newly made dictionary called names
+
+
 for name in names:
 
     if name not in counts:
         counts[name] = 1
     else:
         counts[name] += 1
+
+counts.pop("done")
 print(counts)
+
